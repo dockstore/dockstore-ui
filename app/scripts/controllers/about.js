e357d6e86f0c6db2207ba126df15a1e05599c54f
Vincent Chung <vincent.chung@oicr.on.ca>
2015-10-18 23:17:31 -0400

    Yeoman Angular generated project structure.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dockstoreuiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dockstoreuiApp
+ */
+angular.module('dockstoreuiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
