83b6abdf2bf7f32a78b5389dbb0190e61a13cbd9
Vincent Chung <vincent.chung@oicr.on.ca>
2015-10-26 14:24:02 -0400

    Fix authentication and update README w/ build status.

--- app/scripts/services/webservice.js
+++ app/scripts/services/webservice.js
@@ -13,12 +13,12 @@ angular.module('dockstore.ui')
     API_URI_DEBUG: 'http://www.dockstore.org/tests/dummy-data',
 
     GITHUB_AUTH_URL: 'https://github.com/login/oauth/authorize',
-    GITHUB_CLIENT_ID: '41b685026f5effa17069',
+    GITHUB_CLIENT_ID: '7ad54aa857c6503013ea',
     GITHUB_REDIRECT_URI: 'http://www.dockstore.org/%23/login',
     GITHUB_SCOPE: 'read:org',
     
     QUAYIO_AUTH_URL: 'https://quay.io/oauth/authorize',
-    QUAYIO_CLIENT_ID: 'XWGF22T0VWWL579SFAAD',
+    QUAYIO_CLIENT_ID: 'X5HST9M57O6A57GZFX6T',
     QUAYIO_REDIRECT_URI: 'http://www.dockstore.org/%23/onboarding',
     QUAYIO_SCOPE: 'repo:read,user:read'
   });
