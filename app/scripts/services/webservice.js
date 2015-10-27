8ac9a7c1c778425465e57481068acb3f79fc2d83
Vincent Chung <vincent.chung@oicr.on.ca>
2015-10-27 14:57:02 -0400

    Change the webservice.js configuration to point to localhost. by default

--- app/scripts/services/webservice.js
+++ app/scripts/services/webservice.js
@@ -9,17 +9,17 @@
  */
 angular.module('dockstore.ui')
   .constant('WebService', {
-    API_URI: 'http://www.dockstore.org:8080',
-    API_URI_DEBUG: 'http://www.dockstore.org/tests/dummy-data',
+    API_URI: 'http://localhost:8080',
+    API_URI_DEBUG: 'http://localhost:9000/tests/dummy-data',
 
     GITHUB_AUTH_URL: 'https://github.com/login/oauth/authorize',
-    GITHUB_CLIENT_ID: '7ad54aa857c6503013ea',
-    GITHUB_REDIRECT_URI: 'http://www.dockstore.org/%23/login',
+    GITHUB_CLIENT_ID: 'a70739297a7d67f915de',
+    GITHUB_REDIRECT_URI: 'http://localhost:9000/%23/login',
     GITHUB_SCOPE: 'read:org',
     
     QUAYIO_AUTH_URL: 'https://quay.io/oauth/authorize',
-    QUAYIO_CLIENT_ID: 'X5HST9M57O6A57GZFX6T',
-    QUAYIO_REDIRECT_URI: 'http://www.dockstore.org/%23/onboarding',
+    QUAYIO_CLIENT_ID: 'RWCBI3Y6QUNXDPYKNLMC',
+    QUAYIO_REDIRECT_URI: 'http://localhost:9000/%23/onboarding',
     QUAYIO_SCOPE: 'repo:read,user:read',
 
     DSCLI_RELEASE_URL: 'https://github.com/CancerCollaboratory/dockstore/releases/download/0.0.4/dockstore'
