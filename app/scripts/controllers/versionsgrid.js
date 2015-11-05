6397990f5f556ed249f7dcb3e961d8bdc4f94b33
Vincent Chung <vincent.chung@oicr.on.ca>
2015-11-05 13:14:10 -0500

    Support for size/date modified columns in versionsgrid (when returned by webservice).

--- app/scripts/controllers/versionsgrid.js
+++ app/scripts/controllers/versionsgrid.js
@@ -16,6 +16,33 @@ angular.module('dockstore.ui')
       $scope.sortColumn = 'version';
       $scope.sortReverse = false;
 
+      $scope.getHRSize = function(size) {
+        if (!size) return 'n/a';
+        var hrSize = '';
+        var exp = Math.log(size) / Math.log(2);
+        if (exp < 10) {
+          hrSize = size.toFixed(2) + ' bytes';
+        } else if (exp < 20) {
+          hrSize = (size / Math.pow(2, 10)).toFixed(2) + ' kB';
+        } else if (exp < 30) {
+          hrSize = (size / Math.pow(2, 20)).toFixed(2) + ' MB';
+        } else if (exp < 40) {
+          hrSize = (size / Math.pow(2, 30)).toFixed(2) + ' GB';
+        }
+        return hrSize;
+      };
+
+      $scope.getDateModified = function(timestamp) {
+        if (!timestamp) return 'n/a';
+        var moy = ['Jan.', 'Feb.', 'Mar.', 'Apr.',
+                    'May', 'Jun.', 'Jul.', 'Aug.',
+                    'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+        var dateObj = new Date(timestamp * 1000);
+        return moy[dateObj.getMonth()] + ' ' +
+                dateObj.getDate() + ', ' +
+                dateObj.getFullYear();
+      };
+
       $scope.clickSortColumn = function(columnName) {
         if ($scope.sortColumn === columnName) {
           $scope.sortReverse = !$scope.sortReverse;
