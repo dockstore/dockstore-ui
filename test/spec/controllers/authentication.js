453055d9b8142adaeb75f0e84cc4d3ce9e7f66f1
Vincent Chung <vincent.chung@oicr.on.ca>
2015-11-23 11:42:42 -0500

    Disable unused tests to fix the build.

--- test/spec/controllers/authentication.js
+++ test/spec/controllers/authentication.js
@@ -17,7 +17,7 @@ describe('Controller: AuthenticationCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(AuthenticationCtrl.awesomeThings.length).toBe(3);
-  });
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(AuthenticationCtrl.awesomeThings.length).toBe(3);
+  // });
 });
