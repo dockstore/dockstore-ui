f9868534d78fd47a4ca2b9c6085a6bc2681b6e49
Vincent Chung <vincent.chung@oicr.on.ca>
2015-11-24 16:24:53 -0500

    Fix containerFileViewer regression, did not reload on container selection.

--- app/scripts/directives/containerfileviewer.js
+++ app/scripts/directives/containerfileviewer.js
@@ -18,12 +18,12 @@ angular.module('dockstore.ui')
       },
       templateUrl: 'templates/containerfileviewer.html',
       link: function postLink(scope, element, attrs) {
-        scope.$watch('containerObj.path', function(newValue, oldValue) {
+        scope.$watch('containerObj.path', function(newValue, oldValue) {console.log('new document!');
           if (newValue) scope.setDocument();
         });
         scope.$watchGroup(
-          ['selTagName', 'enabled'],
-          function(newValues, oldValues) {
+          ['selTagName', 'enabled', 'containerObj.id'],
+          function(newValues, oldValues) {console.log('nv', newValues);
             if (newValues[1]) scope.refreshDocument();
         });
       }
