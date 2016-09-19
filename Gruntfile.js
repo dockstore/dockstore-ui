7c27b45acb24cdaee65cddbcd388f37b9a409109
Denis Yuen <denis.yuen@oicr.on.ca>
2016-09-19 15:14:18 -0400

    Add copyright notice to match webservice (#92)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2016 OICR
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 // Generated on 2015-10-19 using generator-angular 0.12.1
 'use strict';
 
@@ -521,11 +537,11 @@ module.exports = function (grunt) {
     //protractor
     protractor: {
       options: {
-        configFile: "e2e.conf.js", // Default config file 
-        keepAlive: true, // If false, the grunt process stops when the test fails. 
-        noColor: false, // If true, protractor will not use colors in its output. 
+        configFile: "e2e.conf.js", // Default config file
+        keepAlive: true, // If false, the grunt process stops when the test fails.
+        noColor: false, // If true, protractor will not use colors in its output.
         args: {
-          // Arguments passed to the command 
+          // Arguments passed to the command
         }
       },
       e2e: {
@@ -579,7 +595,7 @@ module.exports = function (grunt) {
   ]);
 
   grunt.registerTask('build', function(target){
-    
+
     grunt.log.write("revision: "+gitRev);
     grunt.file.write('./app/gitVersion.htm',"<br>DockstoreUI&nbsp-&nbsp"+
       "<a href=\"https://github.com/ga4gh/dockstore-ui/commit/"+gitRev+"\" target=\"_blank\" style=\"color:white\">"+
--- app/scripts/directives/disquscomments.js
+++ app/scripts/directives/disquscomments.js
@@ -1,3 +1,19 @@
+/*
+ *    Copyright 2016 OICR
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 'use strict';
 
 /**
@@ -16,12 +32,12 @@ angular.module('dockstore.ui')
         * RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
         * LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
         */
-        
+
         var disqus_config = function () {
           this.page.url = window.location.href; // Replace PAGE_URL with your page's canonical URL variable
           //this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
         };
-        
+
         (function() { // DON'T EDIT BELOW THIS LINE
           var d = document, s = d.createElement('script');
 
